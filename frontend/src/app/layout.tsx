b1dc04ed94fb0e9fd6cd7d81ea1c093880140dca
gottegubben <lukas.soderberg0404@gmail.com>
2025-11-13 16:03:32 +0100

    Setup some basic stuff... no code yet.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "../../components/header/header";
 
 // The metadata will be extracted by the next.js app and be delivered to the user.
 export const metadata: Metadata = {
@@ -12,6 +13,7 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
   return (
     <html lang="en">
       <body>
+        <Header></Header>
         {children}
       </body>
     </html>
