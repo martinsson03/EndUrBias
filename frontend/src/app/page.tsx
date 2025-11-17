f5394e84a69f35d3787bcff9e19027f5d7a8c40e
gottegubben <lukas.soderberg0404@gmail.com>
2025-11-17 09:15:08 +0100

    Added link and also added color.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center p-10 gap-5">
       <h1 className="text-center">End Your Bias</h1>
-      <p className="text-center max-w-3xl">Our innovative recruiter tool leverages AI to create a fairer, more objective hiring process by removing identifying details that could introduce bias. Critical information such as name, gender, ethnicity, and origin is automatically anonymized, allowing recruiters to evaluate candidates solely on skills, experience, and qualifications. By focusing on merit rather than personal characteristics, this tool helps organizations build more diverse, inclusive, and equitable teams.</p>
+      <p className="text-center text-secondary-foreground max-w-3xl">Our innovative recruiter tool leverages AI to create a fairer, more objective hiring process by removing identifying details that could introduce bias. Critical information such as name, gender, ethnicity, and origin is automatically anonymized, allowing recruiters to evaluate candidates solely on skills, experience, and qualifications. By focusing on merit rather than personal characteristics, this tool helps organizations build more diverse, inclusive, and equitable teams.</p>
 
       <div className="flex flex-row gap-10 pt-10">
         <LoginRedirectCard signInAs="User" redirectUrl="/user"></LoginRedirectCard>
