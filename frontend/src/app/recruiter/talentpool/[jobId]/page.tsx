8317cffbef7adb671d1e9f75235b9160d716e58f
gottegubben <lukas.soderberg0404@gmail.com>
2025-11-18 20:18:47 +0100

    Played around with dynamic (slug) pages.

---
+++ frontend/src/app/recruiter/talentpool/[jobId]/page.tsx
@@ -0,0 +1,5 @@
+import { AppProps } from "next/app";
+
+export default async function JobView({ params }: { params: Promise<{ jobId: string }> }) {
+    return <div><p>JOB ID {(await params).jobId}</p></div>
+};
