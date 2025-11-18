b28f5c098f750569382504fe2ca1a9c9a37fa29b
gottegubben <lukas.soderberg0404@gmail.com>
2025-11-18 19:58:32 +0100

    Added breadcrumbs...

--- frontend/src/app/recruiter/page.tsx
+++ frontend/src/app/recruiter/page.tsx
@@ -1,9 +1,82 @@
 "use client"
 
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+import {
+    Breadcrumb,
+    BreadcrumbEllipsis,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import Link from "next/link";
+
+import { SeperatorVerticle } from "@/components/ui/seperator";
+
+import { Button } from "@/components/ui/button";
+
 export default function Recruiter() {
+    const jobs: job[] = [
+        {
+            title: "Hållbar utveckling",
+            organisation: "Valvi Tech",
+            description: "Förändra världen med sådär-ny teknik."
+        }
+    ];
+
     return (
-        <div>
-            
-        </div>
+        <div className="margin-responsive flex flex-col gap-5 mt-5">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/">Home</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator></BreadcrumbSeparator>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/recruiter">Jobs</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+
+            <h5 className="text-secondary-foreground">{jobs.length} jobs</h5>
+
+            {
+                jobs.map((job, index) => jobCard(job, index))
+            }
+       </div>
+    );
+};
+
+interface job {
+    title: string,
+    organisation: string,
+    description: string
+};
+
+function jobCard(job: job, index: number) {
+    return (
+        <Card className="flex flex-row p-3 pl-6 pr-6 items-center gap-5 justify-between" key={index}>
+            <div className="flex flex-row gap-5 h-full items-center">
+                <h5>{job.title}</h5>
+                <SeperatorVerticle></SeperatorVerticle>
+                <h5 className="text-secondary-foreground">{job.description}</h5>
+            </div>
+            <Link href={`/recruiter/talentpool/${index}`}></Link><Button>View talentpool</Button>
+        </Card>
     );
 };
---
+++ frontend/src/components/ui/seperator.tsx
@@ -0,0 +1,6 @@
+// Small seperator for seperating elements vertically.
+export function SeperatorVerticle() {
+    return (
+        <div className="h-full w-0.5 bg-secondary-foreground"></div>
+    );
+};
