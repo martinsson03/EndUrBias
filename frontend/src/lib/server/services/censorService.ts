4343cf8e8c6d566bc1c5acb411394c013e6a632c
gottegubben <lukas.soderberg0404@gmail.com>
2025-11-21 10:12:47 +0100

    Defined the function that censors the cv!

--- frontend/src/lib/server/services/censorService.ts
+++ frontend/src/lib/server/services/censorService.ts
@@ -1,6 +1,11 @@
 // Censor functions, on the server, that is related to censoring resumes. Contains business logic.
 
+import ApplicationSubmitRequest from "@/lib/models/requests/applicationSubmitRequest";
+import AnonymizeResponse from "@/lib/models/responses/AnonymizeResponse";
+
 // Censors a cv using the censor micro-service and returns the new cv as a string.
-export async function CensorCV(cv: string): Promise<string> {
+export async function CensorCV(request: ApplicationSubmitRequest): Promise<AnonymizeResponse> {
     throw new Error("Not implemented!");
+
+    // Send post request to censorIpAddr/anonymize, with body: AnonymizeRequest {}
 }
