e8f4e1c70ef0bb582fc4dacb3ff6d429de91dc57
gottegubben <lukas.soderberg0404@gmail.com>
2025-11-21 11:10:08 +0100

    Created the database service!

---
+++ frontend/src/lib/server/services/databaseService.ts
@@ -0,0 +1,57 @@
+// The database service that will be used to make queries.
+
+import { Pool, PoolClient, QueryResult } from "pg";
+
+// Define the pool of connections that can make request to the database.
+const pool: Pool = new Pool({
+    user: "",
+    host: "",
+    database: "",
+    password: "",
+    port: 0
+});
+
+// Verifies the connection to the postgres db server.
+async function verifyConnection(): Promise<void> {
+    try {
+        // Attempt to acquire a client from the pool.
+        const client: PoolClient = await pool.connect();
+        console.log('Connected to PostgreSQL database!');
+        client.release(); // Release the client back to the pool.
+    } catch (error) {
+        console.error('Error occurred while connecting to the database: ', error);
+    }
+}
+
+await verifyConnection(); // Verify before continuing!
+
+// Get an sql client from the pool.
+export async function GetSqlClient(): Promise<PoolClient> {
+    return await pool.connect();
+}
+
+// release the sql client.
+export async function ReleaseSqlClient(client: PoolClient): Promise<void> {
+    client.release();
+}
+
+// Function for making a sql query and returning the object.
+export async function MakeSqlQuery<T>(sqlQuery: string): Promise<T[] | undefined> {
+    const client = await GetSqlClient();
+
+    try {
+        const response: QueryResult<any> = await client.query(sqlQuery); // Make the query.
+
+        return response.rows as T[]; // Try to cast it to the type T!
+    }
+    catch (error: any) {
+        console.error("Error when creating querying the db: ", error.message || error);
+
+        return undefined; // If any errors occured, return undefined.
+    }
+    finally {
+        await ReleaseSqlClient(client); // Release the resource.
+    }
+}
+
+export default pool;
