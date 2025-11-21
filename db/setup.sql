5a8fa314d46f38c08348e2943ffe64f966e56b7a
Martin Martinsson <martinsson.martin03@gmail.com>
2025-11-21 11:04:40 +0100

    Created a general setup file

--- db/setup.sql
+++ db/setup.sql
@@ -1,25 +1,59 @@
--- Command to run this file (modify this if needed):
--- psql -f runsetup.sql i.e the whole path to the file example below [location psql.exe] -f [location of this file] [server]
--- &"C:\Program Files\PostgreSQL\16\bin\psql.exe" -f "C:\Users\kph\Chalmers datateknik\datateknik3\databas\task1\runsetup.sql" postgresql://postgres:postgres@127.0.0.1
- 
+CREATE TABLE Companies (
+    id CHAR(8) PRIMARY KEY,
+    logoId CHAR(8),
+    name TEXT,
+    description TEXT
+);
 
--- This script deletes everything in your database
--- this is the only part of the script you do not need to understand
-\set QUIET true
-SET client_min_messages TO WARNING; -- Less talk please.
--- This script deletes everything in your database
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO CURRENT_USER;
--- This line makes psql stop on the first error it encounters
--- You may want to remove this when running tests that are intended to fail
-\set ON_ERROR_STOP ON
-SET client_min_messages TO NOTICE; -- More talk
-\set QUIET false
+CREATE TABLE Users (
+    id CHAR(12) PRIMARY KEY,
+    firstName TEXT,
+    lastName TEXT,
+    gender CHAR(1) CHECK (gender IN ('M','F')),
+    email TEXT,
+    phone CHAR(10),
+    role TEXT CHECK (role IN ('recruiter','candidate','other'))
+);
+
+CREATE TABLE Recruiters (
+    id CHAR(8) PRIMARY KEY,
+    userId CHAR(12) NOT NULL,
+    companyId CHAR(8) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Users(id),
+    FOREIGN KEY (companyId) REFERENCES Companies(id)
+);
+
+CREATE TABLE JobPostings (
+    id CHAR(10) PRIMARY KEY,
+    companyId CHAR(8) NOT NULL,
+    recruiterId CHAR(8) NOT NULL,
+    dateOfTermination TIMESTAMP NOT NULL,
+    title TEXT,
+    location TEXT,
+    extent CHAR(3) DEFAULT '100',
+    description TEXT,
+    tags TEXT,
+    FOREIGN KEY (companyId) REFERENCES Companies(id),
+    FOREIGN KEY (recruiterId) REFERENCES Recruiters(id)
+);
+
+CREATE TABLE Applications (
+    id CHAR(12) PRIMARY KEY,
+    userId CHAR(12) NOT NULL,
+    jobId CHAR(10) NOT NULL,
+    dateSent TIMESTAMP NOT NULL,
+    censuredCV TEXT,
+    cv TEXT,
+    state TEXT CHECK (state IN ()),
+    FOREIGN KEY (userId) REFERENCES Users(id),
+    FOREIGN KEY (jobId) REFERENCES JobPostings(id)
+);
 
 
--- \ir is for include relative, it will run files in the same directory as this file
--- Note that these are not SQL statements but rather Postgres commands (no terminating semicolon). 
-\ir tables.sql
-\ir inserts.sql
-\ir views.sql
+CREATE OR REPLACE VIEW Jobdetails AS
+SELECT jp.dateOfTermination, jp.title, jp.location, jp.extent, jp.description, jp.tags
+FROM JobPostings;
+
+CREATE OR REPLACE VIEW JobviewModel AS
+SELECT * 
+FROM Jobdetails;
--- db/views.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE OR REPLACE VIEW Jobdetails AS
-SELECT jp.dateOfTermination, jp.title, jp.location, jp.extent, jp.description, jp.tags
-FROM JobPostings;
-
-CREATE OR REPLACE VIEW JobviewModel AS
-SELECT * 
-FROM Jobdetails;
