cbf176da415bb8e07eecb2cc799035427099ef7b
gottegubben <lukas.soderberg0404@gmail.com>
2025-11-27 13:23:50 +0100

    Removed db in database models!

--- frontend/src/lib/models/db/dbCompany.ts
+++ frontend/src/lib/models/db/dbCompany.ts
@@ -2,7 +2,7 @@ import type { id } from "../shared/id";
 import IdentifiableObject from "../shared/identifiableObject";
 
 // The company data object that exist in the database.
-export default interface dbCompany extends IdentifiableObject {
+export default interface Company extends IdentifiableObject {
     Name: string,        // Name of company.
     CompanyLogoId: id,   // Id of the logo associated with the company.
     Description: string, // Text explaining what the company does etc.
--- frontend/src/lib/models/db/dbUser.ts
+++ frontend/src/lib/models/db/dbUser.ts
@@ -2,7 +2,7 @@ import type { UserRole } from "../shared/userRole";
 import IdentifiableObject from "../shared/identifiableObject";
 
 // As seen in the database. The core data object representing any user on the app.
-export default interface dbUser extends IdentifiableObject {
+export default interface User extends IdentifiableObject {
     FirstName: string,
     LastName: string,
     Gender: string,
