6eee469a0605b347f15c3cfeccf7002a42266cb2
gottegubben <lukas.soderberg0404@gmail.com>
2025-11-28 14:08:06 +0100

    Added user sex (gender) to the models!

--- frontend/src/lib/models/db/dbUser.ts
+++ frontend/src/lib/models/db/dbUser.ts
@@ -1,11 +1,12 @@
 import type { UserRole } from "../shared/userRole";
 import IdentifiableObject from "../shared/identifiableObject";
+import { UserSex } from "../shared/userSex";
 
 // As seen in the database. The core data object representing any user on the app.
 export default interface User extends IdentifiableObject {
     FirstName: string,
     LastName: string,
-    Gender: string,
+    Gender: UserSex,
     Mail: string,
     Phonenumber: string,
     Role: UserRole
